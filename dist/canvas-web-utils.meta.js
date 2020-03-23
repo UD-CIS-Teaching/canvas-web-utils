ee6fd562ea1d5d49cec957838965fcba78a3af55
Austin Cory Bart <acbart@vt.edu>
2020-03-23 14:59:12 -0400

    Build first version!

---
+++ dist/canvas-web-utils.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        canvas-web-utils
+// @version     1.0.0
+// @author      acbart@udel.edu
+// @description Tools for controlling Canvas online
+// @homepage    https://github.com/UD-CIS-Teaching/canvas-web-utils#readme
+// @supportURL  https://github.com/UD-CIS-Teaching/canvas-web-utils/issues
+// @match       https://*.instructure.com/courses/*
+// @grant       none
+// @updateURL   https://github.com/UD-CIS-Teaching/canvas-web-utils/blob/master/dist/canvas-web-utils.user.js
+// @downloadURL https://github.com/UD-CIS-Teaching/canvas-web-utils/blob/master/dist/canvas-web-utils.user.js
+// ==/UserScript==
--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,9 @@ module.exports = {
             headers: {
                 version: `[version]`,
                 match: "https://*.instructure.com/courses/*",
-                grant: "none"
+                grant: "none",
+                updateURL: "https://github.com/UD-CIS-Teaching/canvas-web-utils/blob/master/dist/canvas-web-utils.user.js",
+                downloadURL: "https://github.com/UD-CIS-Teaching/canvas-web-utils/blob/master/dist/canvas-web-utils.user.js"
             }
         })
     ]
